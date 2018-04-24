31850596aba313ad9d4247f3501898f2b704132f
Dan Nilsson <raxptor@gmail.com>
2018-04-24 10:52:43 +0200

    - first translation

---
+++ runtime/csharp/Translation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Putki;
+
+namespace Putki
+{
+    public interface Translation
+    {
+        string Translate(string text, string category);
+        string Translate(string text, string category, params object[] args);
+    }
+}
