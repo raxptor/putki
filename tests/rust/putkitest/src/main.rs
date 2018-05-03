60a19f96c09d52323bb19478d2f04f89ec846269
Dan Nilsson <raxptor@gmail.com>
2018-05-03 03:15:00 +0200

    - more tests

--- tests/rust/putkitest/src/main.rs
+++ tests/rust/putkitest/src/main.rs
@@ -7,6 +7,9 @@ use std::rc::Rc;
 use std::rc::Weak;
 use std::cell::Cell;
 use std::cell::RefCell;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 pub struct FirstType
 {
@@ -48,8 +51,122 @@ pub fn check(ct: &ChildType)
     }
 }
 
+struct KeyValue
+{
+    key: String,
+    value: String
+}
+
+fn parse_list<F>(data: &str, parser:F, term:char) -> &str
+    where F: Fn(&str) -> &str
+{
+    let mut cur = data;
+    let mut it = data.char_indices().enumerate(); 
+    let mut hx = 0;    
+    loop {
+        match it.next() {
+            None => return "",
+            Some(ref x) => {                 
+                let value = &x.1;
+                if value.1 == term {
+                    return &cur[value.0 ..];
+                } else if !value.1.is_whitespace() {
+                    cur = parser(cur);
+                    it = cur.char_indices().enumerate();
+                }
+            }
+        }
+    }
+    return "";
+}
+
+// Parse one @type id { block }
+fn parse_object<'a>(data: &'a str, default_type: &str) -> &'a str
+{
+    let mut cur = data;
+    let mut it = data.char_indices().enumerate(); 
+    let type_begin = 0;
+    let mut type_end = 0;
+    loop {
+        match it.next() {
+            None => return "",
+            Some(ref x) => {                 
+                let value = &x.1;
+                if (value.1.is_whitespace() && type_end == 0)
+                {
+                    type_end = value.0;
+                }
+                if (value.1 == '{')
+                {
+                    let content_begin = value.0;
+                    let obj_type = String::from(&cur[0..type_end]);
+                    let name = String::from(&cur[type_end .. content_begin]);                    
+                    println!("type=[{}] name=[{}]", obj_type.trim(), name.trim());
+                    match it.next() {
+                        None => println!("Unexpected end of file"),
+                        Some(ref nextval) => {
+                            println!("parisng at {} [{}]", value.0, &cur[nextval.0 .. (nextval.0 + 4)]);
+                            return parse_list(&cur[nextval.0 .. ], |pd:&str| -> &str {
+                                println!("field[{}]", &pd[0..1]);
+                                return &pd[1 ..];    
+                            }, '}');
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn parse_file(data: &str)
+{
+    let mut cur = data;
+    let mut it = data.char_indices().enumerate(); 
+    let mut hx = 0;    
+    loop {
+        match it.next() {
+            None => return,
+            Some(ref x) => {                 
+                let value = &x.1;
+                if value.1 == '@' {
+                    cur = parse_object(&cur[value.0..], "");
+                    it = cur.char_indices().enumerate();
+                    hx = hx + 1;
+                    if (hx > 4) {
+                        return;
+                    }
+                }
+            } 
+        }
+    }
+
+//    loop {
+        /*
+        match it {
+            None => break,
+            Some(x) => it = x.next()
+        }
+        */
+    //}
+/*    for (index, ch) in data.char_indices() {
+        if (ch == '@') {
+            println!("object at {}!", index);
+        }
+    }
+    */
+}
+
 pub fn main() 
-{    
+{   
+    let mut contents = String::new();
+    { 
+        let mut f = File::open("data/main.txt").expect("file not found");    
+        f.read_to_string(&mut contents).expect("something went wrong reading the file");
+    }
+    parse_file(&contents);
+    //println!("file contains: [{}]", contents);
+/*
+
     let k = ChildType::Type1(Box::new(FirstType {
         gurka: 3,
         kurka: 9
@@ -62,21 +179,5 @@ pub fn main()
     println!("size is {} ", size_of::<Parent>());
     println!("size is {} ", size_of::<FirstType>());
     println!("size of box {} ", size_of::<Box<FirstType>>());
-
-/*
-    let mut np = Pa
-    {
-        ch: Some(c)
-    };
-*/
-    //ch.p.ch = Some(&ch);
-
-    /*
-    let mut x = Vec::new();
-    x.push(1);
-    x.push(2);
-    let sl:&[i32] = (&x);
-
-    let k:Vec<i32> = sl.to_owned();
     */
 }
